06758a630201521f6bdb4921582dd773e31eaa06
costar011 <dnaer456@gmail.com>
2021-11-29 17:58:08 +0900

    tuning react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello</p>
+        <p>My GitHub costar011</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
